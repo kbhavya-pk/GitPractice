75c4060602ee674f80458c101e86963765a7bdd5
kbhavya <kbhavya@KBhavya-PC.pkrmsezdc.com>
2018-03-12 16:00:49 +0530

    Made changes to the Interation class with Sysout operation

---
+++ src/com/prok/java/Iteration.java
@@ -0,0 +1,25 @@
+package com.prok.java;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Iteration {
+	
+	    public static void main(String a[]){
+	         
+	        List<String> myList = new ArrayList<String>();
+	        myList.add("Java");
+	        myList.add("Unix");
+	        myList.add("Oracle");
+	        myList.add("C++");
+	        myList.add("Perl");
+	        Iterator<String> itr = myList.iterator();
+	        while(itr.hasNext()){
+	            System.out.println(itr.next());
+	            System.out.println("Made a change");
+	        
+	    }
+	}
+
+}
